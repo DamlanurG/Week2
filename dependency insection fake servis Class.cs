c38fbcdbf95fac367ba65c62baa164bb6a7b8b75
Damla Nur Güney <138982566+DamlanurG@users.noreply.github.com>
2023-08-04 21:17:45 +0300

    Add files via upload

---
+++ dependency insection fake servis Class.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DamlaNurGuney.Sipay.Hafta1
+    //interface oluşturmak için
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+    }
+
+
+    public class dependency_insection_fake_servis_Class
+    {
+        public class FakeProductService : IProductService
+        {
+            private readonly List<Product> products;
+
+            public FakeService() => products = new List<Product>
+
+                {
+        if (Products.Count == 0)
+            for (int i = 1; i< 10; i++)
+            {
+                var product = new Product.Product
+                {
+                    Id = i,
+                    Name = $"Product-{i}",
+                    Prize = i * 10
+                };
+
+          
+            }
+
+
+    }
+
+
+      
+        };
+
+
+
+
+
+        }
+    }
---
+++ extension Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DamlaNurGuney.Sipay.Hafta1
+{ //gelen para birimi değerini ToFormattedPrice() metodu ile para birimi formatında oluşturması için metod oluşturuldu.
+    public static class extension_Class
+    {
+        public static decimal price ToFormattedPrice(this decimal amount)
+        {
+            return amount.ToString("#,##0.00");
+        }
+
+        decimal amount = 99999999;
+
+        string moneyFormat = Amount.ToFormattedPrice();
+
+        public global::System.Decimal Amount { get => amount; set => amount = value; }
+    }
